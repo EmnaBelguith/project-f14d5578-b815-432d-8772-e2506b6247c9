9de9b29010f4c0530cd479b1e98ca658abf04386
msi <msi@DESKTOP-SKMGR3V>
2025-08-04 15:22:48 +0100

    Add project files and GitHub Actions workflow for Process ID: f14d5578-b815-432d-8772-e2506b6247c9

---
+++ spring-test/src/main/java/org/springframework/test/web/reactive/server/StatusAssertions.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2002-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.test.web.reactive.server;
+
+import org.springframework.http.HttpStatusCode;
+import org.springframework.test.web.support.AbstractStatusAssertions;
+
+/**
+ * Assertions on the response status.
+ *
+ * @author Rossen Stoyanchev
+ * @author Rob Worsnop
+ * @since 5.0
+ * @see WebTestClient.ResponseSpec#expectStatus()
+ */
+public class StatusAssertions extends AbstractStatusAssertions<ExchangeResult, WebTestClient.ResponseSpec> {
+
+
+	StatusAssertions(ExchangeResult result, WebTestClient.ResponseSpec spec) {
+		super(result, spec);
+	}
+
+
+	@Override
+	protected HttpStatusCode getStatus() {
+		return getExchangeResult().getStatus();
+	}
+
+	@Override
+	protected void assertWithDiagnostics(Runnable assertion) {
+		getExchangeResult().assertWithDiagnostics(assertion);
+	}
+
+}
